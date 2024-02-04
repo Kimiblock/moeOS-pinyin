f5bdc1b7fab038c5bf942b2456ab26fb05da480f
Kimiblock Moe <kimiblock@icloud.com>
2024-02-04 14:29:28 +0800

    feat: Disable faulty script

--- rime-data/rime.lua
+++ rime-data/rime.lua
@@ -17,9 +17,6 @@ corrector = require("corrector")
 -- 自动大写英文词汇
 autocap_filter = require("autocap_filter")
 
--- 降低部分英语单词在候选项的位置，可在方案中配置要降低的单词
-reduce_english_filter = require("reduce_english_filter")
-
 
 
 
