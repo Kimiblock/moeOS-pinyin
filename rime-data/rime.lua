980eced07e58be24f1408ff0541088a08b99447e
Kimiblock Moe <kimiblock@icloud.com>
2024-06-02 10:52:15 +0800

    Add missing config

--- rime-data/rime.lua
+++ rime-data/rime.lua
@@ -5,9 +5,6 @@
 
 -- processors:
 
--- 以词定字，可在 default.yaml key_binder 下配置快捷键，默认为左右中括号 [ ]
-select_character = require("others.rime-ice.lua.select_character")
-
 limit_mulchar = require("limit_mulchar")
 
 -- filters:
@@ -28,9 +25,7 @@ autocap_filter = require("others.rime-ice.lua.autocap_filter")
 -- 在方案里写配置项:
 -- 提升 count 个词语，插入到第 idx 个位置。
 -- 示例：将 2 个词插入到第 4、5 个候选项，输入 jie 得到「1接 2解 3姐 4饥饿 5极恶」
--- long_word_filter:
---   count: 2
---   idx: 4
+
 long_word_filter = require("others.rime-ice.lua.long_word_filter")
 
 -- 根据是否在用户词典，在 comment 上加上一个星号 *
