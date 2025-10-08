624f75aa173411566addcba97f329a3e2f22e65e
Kimiblock Moe <kimiblock@icloud.com>
2025-10-08 16:26:07 +0800

    Strip wanxiang

--- release.sh
+++ release.sh
@@ -5,8 +5,19 @@ function grabSources() {
 	echo "Downloading wanxiang gram..."
 	curl \
 		"${url}/releases/download/LTS/wanxiang-lts-zh-hans.gram" \
+		--progress-bar \
 		-Lo \
 		rime-data/others/wanxiang.gram
+	echo "Downloading ICE dicts..."
+	local url="https://raw.githubusercontent.com/iDvel/rime-ice"
+	mkdir -p rime-data/others/rime-ice/
+	for file in 8105 base ext others tencent; do
+		curl \
+			"${url}/refs/heads/main/cn_dicts/${file}.dict.yaml" \
+			--progress-bar \
+			-Lo \
+			"rime-data/others/rime-ice/${file}.dict.yaml"
+	done
 
 }
 
