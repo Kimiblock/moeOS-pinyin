a59122d0c3e102ec5c88ae187f7e46fda7ea10fb
Kimiblock Moe <kimiblock@icloud.com>
2026-06-30 13:05:15 +0800

    lua: vastly simplify

--- rime-data/rime.lua
+++ rime-data/rime.lua
@@ -6,38 +6,11 @@
 -- processors:
 
 limit_mulchar = require("limit_mulchar")
-
--- 以词定字，可在 default.yaml → key_binder 下配置快捷键，默认为左右中括号 [ ]
-select_character = require("select_character")
-
--- translators:
-
--- 日期时间，可在方案中配置触发关键字。
-date_translator = require("date_translator")
-
--- 农历，可在方案中配置触发关键字。
-lunar = require("lunar")
-
--- Unicode，U 开头
-unicode = require("unicode")
-
--- 数字、人民币大写，R 开头
-number_translator = require("number_translator")
-
 -- filters:
 
 -- 错音错字提示
 -- 关闭此 Lua 时，同时需要关闭 translator/spelling_hints，否则 comment 里都是拼音
 corrector = require("corrector")
-
--- v 模式 symbols 优先（全拼）
-v_filter = require("v_filter")
-
--- 自动大写英文词汇
-autocap_filter = require("autocap_filter")
-
--- 降低部分英语单词在候选项的位置，可在方案中配置要降低的模式和单词
-reduce_english_filter = require("reduce_english_filter")
 
 -- 辅码，https://github.com/mirtlecn/rime-radical-pinyin/blob/master/search.lua.md
 search = require("search")
@@ -47,8 +20,6 @@ pin_cand_filter = require("pin_cand_filter")
 
 -- 长词优先（全拼）
 long_word_filter = require("long_word_filter")
-
--- 默认未启用：
 
 
 -- 根据是否在用户词典，在 comment 上加上一个星号 *
@@ -75,16 +46,4 @@ cold_word_drop_filter = require("cold_word_drop.filter")
 function force_gc()
     -- collectgarbage()
     collectgarbage("step")
-end
-
--- 临时用的
-function debug_checker(input, env)
-    for cand in input:iter() do
-        yield(ShadowCandidate(
-            cand,
-            cand.type,
-            cand.text,
-            env.engine.context.input .. " - " .. env.engine.context:get_preedit().text .. " - " .. cand.preedit
-        ))
-    end
 end
